862b6ba8a7733ea307729a84d049c304955e5690
Ajay Merchia <ajaymerchia@berkeley.edu>
2018-05-17 21:50:49 -0700

    real estate site

--- app.js
+++ app.js
@@ -20,11 +20,33 @@ app.use(express.static(__dirname + '/views'));
 app.get('/', function (req, res) {
   res.render('index')
 })
+app.get('/index', function (req, res) {
+  res.render('index')
+})
+app.get('/dev', function (req, res) {
+  res.render('dev')
+})
+app.get('/entre', function (req, res) {
+  res.render('entre')
+})
+app.get('/finearts', function (req, res) {
+  res.render('finearts')
+})
+app.get('/prof', function (req, res) {
+  res.render('prof')
+})
+app.get('/realestate', function (req, res) {
+  res.render('realestate')
+})
+
 
 
 app.post('/index', function (req, res) {
   res.render('index')
 })
+
+
+
 
 
 app.listen(port, function () {
